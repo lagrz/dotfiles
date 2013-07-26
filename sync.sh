5f8bfc0dc661cf52ff387a608ec2e9e700b06540
lagrz <roarbort@gmail.com>
2013-07-27 00:57:28 +0400

    update sync file

--- sync.sh
+++ sync.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 #cd "$(dirname "$0")"
-git pull
-git submodule init
-git submodule update
 
 function doIt() {
 	rsync --exclude "etc/" --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" --exclude "readme.md" -av . ~
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
+elif ["$1" == "--update" -o "$1" == "-o"]; then
+	doIt
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		git pull
+		git submodule init
+		git submodule update
 		doIt
 	fi
 fi
